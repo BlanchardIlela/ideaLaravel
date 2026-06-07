f61f914f9cfd91f9ecba513fcc9dab018f913ecc
BlanchardIlela <blancilela@gmail.com>
2026-06-07 15:51:05 +0100

    Affichage de la navigation   @auth : Losque je suis connecté je perds mes (Login et Sign In)  @guest : Losque je ne suis pas connecté

---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,26 @@
+<nav class="border-b border-border px-6">
+    <div class="max-w-7xl mx-auto h-16 flex items-center justify-between">
+        <div>
+            <a href="">
+                <img src="/images/images.jpeg" alt="Idea logo" width="30">
+            </a>
+        </div>
+
+        <div class="flex gap-x-5 items-center">
+
+            @auth
+                <form action="/logout" method="POST">
+                    @csrf
+
+                    <button type="submit">Log Out</button>
+                </form>
+            @endauth
+
+            @guest
+                <a href="login">Sign In</a>
+                <a href="/register" class="btn">Register</a>
+            @endguest
+        </div>
+    </div>
+
+</nav>
